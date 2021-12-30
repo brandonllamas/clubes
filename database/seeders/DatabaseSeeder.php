3460190ed70e7c6b927a3eb7e6acca53a9d9c32d
Brandon Llamas Larios <brandonllamaslarios@gmail.com>
2021-12-30 15:06:43 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,20 +16,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //cree admnistrador
+        $user = new User();
+        $user->name = "Admin ";
+        $user->lastname = "Global";
+        $user->email = "admin@gmail.com";
+        $user->password = Hash::make('123');
+        $user->profile = 1;
+        $user->state = 1;
+        $user->save();
+        // $user->assignRole('Admin_Global');
+
+        //crear Usuario de prueba
+        $user = new User();
+        $user->name = "Brandon ";
+        $user->lastname = "Llamas Larios";
+        $user->email = "brandonllamaslarios@gmail.com";
+        $user->password = Hash::make('123');
+        $user->profile = 2;
+        $user->state = 1;
+        $user->save();
         // \App\Models\User::factory(10)->create();
         $this->truncate();
 
         $this->call([
             ParametersSeeder::class, // Parametros
             ParameterValuesSeeder::class, // Valores parametros
-            userSeed::class, //Usuarios
+            // userSeed::class, //Usuarios
             categoriaSeed::class //categoriaSeed
         ]);
         User::factory(20)->create();
-
     }
 
-    private function truncate(){
+    private function truncate()
+    {
         // Nombre de tablas para eliminar antes de accionarse el seeder
         $tables = [
             'parameters',
