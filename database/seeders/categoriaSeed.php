771a60b07f316ef5c9d47454a0bdb5bad8620a8a
Brandon Llamas Larios <brandonllamaslarios@gmail.com>
2021-12-30 15:08:24 -0500

    Update categoriaSeed.php

--- database/seeders/categoriaSeed.php
+++ database/seeders/categoriaSeed.php
@@ -15,6 +15,26 @@ class categoriaSeed extends Seeder
     public function run()
     {
         //
+        $user = new User();
+        $user->name = "Admin ";
+        $user->lastname = "Global";
+        $user->email = "admin@gmail.com";
+        $user->password = Hash::make('123');
+        $user->profile = 1;
+        $user->state = 1;
+        $user->save();
+        // $user->assignRole('Admin_Global');
+
+        //crear Usuario de prueba
+        $user = new User();
+        $user->name = "Brandon ";
+        $user->lastname = "Llamas Larios";
+        $user->email = "brandonllamaslarios@gmail.com";
+        $user->password = Hash::make('123');
+        $user->profile = 2;
+        $user->state = 1;
+        $user->save();
+
         $categoria = new categorias();
         $categoria->categoria_name = "Comida";
         $categoria->descripcion = "";
